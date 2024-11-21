99d59900f1f08b80e86e0530d0ba339a1c6e71b3
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-21 19:57:11 +0700

    Create Program.cs

---
+++ BMICheck/BMICheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+
+            Console.Write("Please enter a height in meters: ");
+            height = float.Parse(Console.ReadLine());
+            Console.Write("Please enter your weight: ");
+            weight = float.Parse(Console.ReadLine());
+
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+
+            Console.Write("BMI: " + bmi + ", ");
+
+            if (bmi < 18)
+                Console.WriteLine("Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine("Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine("Overweight");
+            else
+                Console.WriteLine("Obese");
+
+            Console.ReadKey();
+        }
+    }
+}
